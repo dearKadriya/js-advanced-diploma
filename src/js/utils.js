29a50b2261dead4c75f0deaab26c6160cb04192a
artemkosnikov <Boxberry123>
2021-06-16 15:36:04 +0300

    calcTileTypes and tests added

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,6 +1,26 @@
 export function calcTileType(index, boardSize) {
+  const cellsOnBoard = boardSize * boardSize;
+  switch (true) {
+    case index === 0:
+      return 'top-left'
+    case ((index + 1) === boardSize):
+      return 'top-right'
+    case index < boardSize:
+      return 'top'
+    case ((index + 1) === cellsOnBoard):
+        return 'bottom-right'
+    case (index === (cellsOnBoard - boardSize)):
+      return 'bottom-left'
+    case ((index % boardSize) === 0):
+      return 'left'
+    case ((index + 1) % boardSize === 0):
+      return 'right'
+    case (index > (cellsOnBoard - boardSize)):
+      return 'bottom'
+    default:
+      return 'center'
+  }
   // TODO: write logic here
-  return 'center';
 }
 
 export function calcHealthLevel(health) {
